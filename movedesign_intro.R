8f9ef4b5adf41858671899ad3154e99e1e2485fb
Andrew Wilk <awilk@rewi.org>
2024-11-20 12:13:43 -0500

    simple script for movement power analysis

---
+++ movedesign_intro.R
@@ -0,0 +1,7 @@
+# INtroduction to teh moveDesign library and shiny app for power analyses
+#install.packages("remotes")
+remotes::install_github("ecoisilva/movedesign")
+
+library(movedesign)
+
+movedesign::run_app()
